373c5dd9c3f44d16de08b8af19d375bf506954d8
Darren Ethier <darren@roughsmootheng.in>
2014-04-16 14:55:52 -0400

    add tests stuff for EE4 cal [touch: 5697]

---
+++ tests/includes/loader.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Loader for EE Calendar Unit Tests initializes plugin and gets thing off to a start.
+ *
+ * @since 		3.1.0
+ * @package 		Event Espresso Calendar
+ * @subpackage 	tests
+ */
+
+// Bootstrap EE
+require EECAL_PLUGIN_DIR . 'EE_Calendar.php';
