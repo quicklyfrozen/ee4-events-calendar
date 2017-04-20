93c6600baf0a361e1d030da7fc603b42a06c0782
Darren Ethier <darren@roughsmootheng.in>
2017-04-20 14:28:29 -0400

    update bootstrap and config for tests in eea-calendar [touch: 10623]

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,26 +1,21 @@
 <?php
 /**
- * Bootstrap for EE4 Calendar Unit Tests
- *
- * @since 		3.1.0
- * @package 		Event Espresso Calendar
- * @subpackage 	Tests
+ * Bootstrap for eea-calendar tests
  */
 
-require( dirname( __FILE__ ) . '/includes/define-constants.php' );
-if ( ! is_readable( WP_TESTS_DIR . '/includes/functions.php' ) ) {
-	die( "The WordPress PHPUnit test suite could not be found.\n" );
-}
+use EETests\bootstrap\AddonLoader;
 
-require_once WP_TESTS_DIR . '/includes/functions.php';
+$core_tests_dir = dirname(dirname(dirname(__FILE__))) . '/event-espresso-core/tests/';
+require $core_tests_dir . 'includes/CoreLoader.php';
+require $core_tests_dir . 'includes/AddonLoader.php';
 
-function _install_and_load_core_and_ee_cal() {
-	require EE_TESTS_DIR . 'includes/loader.php';
-	require EECAL_TESTS_DIR . 'includes/loader.php';
-}
-tests_add_filter( 'muplugins_loaded', '_install_and_load_core_and_ee_cal' );
+define('EECAL_PLUGIN_DIR', dirname(dirname(__FILE__)) . '/');
+define('EECAL_TESTS_DIR', EECAL_PLUGIN_DIR . 'tests');
 
-require WP_TESTS_DIR . '/includes/bootstrap.php';
 
-//Load the EE_specific testing tools
-require EE_TESTS_DIR . 'includes/EE_UnitTestCase.class.php';
+$addon_loader = new AddonLoader(
+    EECAL_TESTS_DIR,
+    EECAL_PLUGIN_DIR,
+    'espresso_calendar.php'
+);
+$addon_loader->init();
