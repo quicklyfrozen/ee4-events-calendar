bb13da2973a59b725c1921dfee5ec48181d8f010
Darren Ethier <darren@roughsmootheng.in>
2017-04-22 16:01:33 -0400

    update test bootstrap/config for ee calendar add-on [touch: 10623]

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,11 +6,15 @@
 use EETests\bootstrap\AddonLoader;
 
 $core_tests_dir = dirname(dirname(dirname(__FILE__))) . '/event-espresso-core/tests/';
+//if still don't have $core_tests_dir, then let's check tmp folder.
+if (! is_dir($core_tests_dir)) {
+    $core_tests_dir = '/tmp/event-espresso-core/tests/';
+}
 require $core_tests_dir . 'includes/CoreLoader.php';
 require $core_tests_dir . 'includes/AddonLoader.php';
 
 define('EECAL_PLUGIN_DIR', dirname(dirname(__FILE__)) . '/');
-define('EECAL_TESTS_DIR', EECAL_PLUGIN_DIR . 'tests');
+define('EECAL_TESTS_DIR', EECAL_PLUGIN_DIR . 'tests/');
 
 
 $addon_loader = new AddonLoader(
